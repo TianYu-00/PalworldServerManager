6a4331b1f32bf4a88b394b1df967754b3a001eaf
Tian <66271788+TianYu-00@users.noreply.github.com>
2024-01-27 18:33:43 +0000

    Added RCON (Has disconnection issue)
    Added RCON, has disconnection issue but clicking disconnect and connect again would work.

--- PalworldServerManager/PalworldServerManager/Form_RCON.Designer.cs
+++ PalworldServerManager/PalworldServerManager/Form_RCON.Designer.cs
@@ -29,45 +29,69 @@ namespace PalworldServerManager
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_RCON));
             panel1 = new System.Windows.Forms.Panel();
-            richTextBox1 = new System.Windows.Forms.RichTextBox();
-            tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            panel2 = new System.Windows.Forms.Panel();
+            richTextBox_output = new System.Windows.Forms.RichTextBox();
+            tableLayoutPanel_sendSection = new System.Windows.Forms.TableLayoutPanel();
             textBox_commandText = new System.Windows.Forms.TextBox();
             button_sendCommand = new System.Windows.Forms.Button();
-            tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
-            textBox1 = new System.Windows.Forms.TextBox();
-            textBox2 = new System.Windows.Forms.TextBox();
+            panel_playersSection = new System.Windows.Forms.Panel();
+            panel_playerListSection = new System.Windows.Forms.Panel();
+            tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            button_kick = new System.Windows.Forms.Button();
+            button_ban = new System.Windows.Forms.Button();
+            panel3 = new System.Windows.Forms.Panel();
+            label1 = new System.Windows.Forms.Label();
+            tableLayoutPanel_topSection = new System.Windows.Forms.TableLayoutPanel();
+            textBox_ipRCON = new System.Windows.Forms.TextBox();
+            textBox_portRCON = new System.Windows.Forms.TextBox();
             button_connectRCON = new System.Windows.Forms.Button();
             button_disconnectRCON = new System.Windows.Forms.Button();
-            textBox3 = new System.Windows.Forms.TextBox();
+            textBox_passwordRCON = new System.Windows.Forms.TextBox();
+            button1 = new System.Windows.Forms.Button();
+            toolTip1 = new System.Windows.Forms.ToolTip(components);
             panel1.SuspendLayout();
-            tableLayoutPanel2.SuspendLayout();
-            tableLayoutPanel3.SuspendLayout();
+            panel2.SuspendLayout();
+            tableLayoutPanel_sendSection.SuspendLayout();
+            panel_playersSection.SuspendLayout();
+            tableLayoutPanel1.SuspendLayout();
+            panel3.SuspendLayout();
+            tableLayoutPanel_topSection.SuspendLayout();
             SuspendLayout();
             // 
             // panel1
             // 
-            panel1.Controls.Add(richTextBox1);
-            panel1.Controls.Add(tableLayoutPanel2);
-            panel1.Controls.Add(tableLayoutPanel3);
+            panel1.Controls.Add(panel2);
+            panel1.Controls.Add(panel_playersSection);
+            panel1.Controls.Add(tableLayoutPanel_topSection);
             resources.ApplyResources(panel1, "panel1");
             panel1.Name = "panel1";
             // 
-            // richTextBox1
+            // panel2
             // 
-            resources.ApplyResources(richTextBox1, "richTextBox1");
-            richTextBox1.Name = "richTextBox1";
+            panel2.Controls.Add(richTextBox_output);
+            panel2.Controls.Add(tableLayoutPanel_sendSection);
+            resources.ApplyResources(panel2, "panel2");
+            panel2.Name = "panel2";
             // 
-            // tableLayoutPanel2
+            // richTextBox_output
             // 
-            resources.ApplyResources(tableLayoutPanel2, "tableLayoutPanel2");
-            tableLayoutPanel2.Controls.Add(textBox_commandText, 0, 0);
-            tableLayoutPanel2.Controls.Add(button_sendCommand, 1, 0);
-            tableLayoutPanel2.Name = "tableLayoutPanel2";
+            resources.ApplyResources(richTextBox_output, "richTextBox_output");
+            richTextBox_output.Name = "richTextBox_output";
+            richTextBox_output.ReadOnly = true;
+            // 
+            // tableLayoutPanel_sendSection
+            // 
+            resources.ApplyResources(tableLayoutPanel_sendSection, "tableLayoutPanel_sendSection");
+            tableLayoutPanel_sendSection.Controls.Add(textBox_commandText, 0, 0);
+            tableLayoutPanel_sendSection.Controls.Add(button_sendCommand, 1, 0);
+            tableLayoutPanel_sendSection.Name = "tableLayoutPanel_sendSection";
             // 
             // textBox_commandText
             // 
+            textBox_commandText.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             resources.ApplyResources(textBox_commandText, "textBox_commandText");
             textBox_commandText.Name = "textBox_commandText";
             // 
@@ -76,43 +100,101 @@ namespace PalworldServerManager
             resources.ApplyResources(button_sendCommand, "button_sendCommand");
             button_sendCommand.Name = "button_sendCommand";
             button_sendCommand.UseVisualStyleBackColor = true;
+            button_sendCommand.Click += button_sendCommand_Click;
             // 
-            // tableLayoutPanel3
+            // panel_playersSection
             // 
-            resources.ApplyResources(tableLayoutPanel3, "tableLayoutPanel3");
-            tableLayoutPanel3.Controls.Add(textBox1, 0, 1);
-            tableLayoutPanel3.Controls.Add(textBox2, 1, 1);
-            tableLayoutPanel3.Controls.Add(button_connectRCON, 0, 0);
-            tableLayoutPanel3.Controls.Add(button_disconnectRCON, 1, 0);
-            tableLayoutPanel3.Controls.Add(textBox3, 1, 2);
-            tableLayoutPanel3.Name = "tableLayoutPanel3";
+            panel_playersSection.Controls.Add(panel_playerListSection);
+            panel_playersSection.Controls.Add(tableLayoutPanel1);
+            panel_playersSection.Controls.Add(panel3);
+            resources.ApplyResources(panel_playersSection, "panel_playersSection");
+            panel_playersSection.Name = "panel_playersSection";
             // 
-            // textBox1
+            // panel_playerListSection
             // 
-            resources.ApplyResources(textBox1, "textBox1");
-            textBox1.Name = "textBox1";
+            resources.ApplyResources(panel_playerListSection, "panel_playerListSection");
+            panel_playerListSection.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            panel_playerListSection.Name = "panel_playerListSection";
             // 
-            // textBox2
+            // tableLayoutPanel1
             // 
-            resources.ApplyResources(textBox2, "textBox2");
-            textBox2.Name = "textBox2";
+            resources.ApplyResources(tableLayoutPanel1, "tableLayoutPanel1");
+            tableLayoutPanel1.Controls.Add(button_kick, 0, 0);
+            tableLayoutPanel1.Controls.Add(button_ban, 1, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            // 
+            // button_kick
+            // 
+            resources.ApplyResources(button_kick, "button_kick");
+            button_kick.Name = "button_kick";
+            button_kick.UseVisualStyleBackColor = true;
+            button_kick.Click += button_kick_Click;
+            // 
+            // button_ban
+            // 
+            resources.ApplyResources(button_ban, "button_ban");
+            button_ban.Name = "button_ban";
+            button_ban.UseVisualStyleBackColor = true;
+            button_ban.Click += button_ban_Click;
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(label1);
+            resources.ApplyResources(panel3, "panel3");
+            panel3.Name = "panel3";
+            // 
+            // label1
+            // 
+            label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(label1, "label1");
+            label1.Name = "label1";
+            // 
+            // tableLayoutPanel_topSection
+            // 
+            resources.ApplyResources(tableLayoutPanel_topSection, "tableLayoutPanel_topSection");
+            tableLayoutPanel_topSection.Controls.Add(textBox_ipRCON, 0, 1);
+            tableLayoutPanel_topSection.Controls.Add(textBox_portRCON, 1, 1);
+            tableLayoutPanel_topSection.Controls.Add(button_connectRCON, 0, 0);
+            tableLayoutPanel_topSection.Controls.Add(button_disconnectRCON, 1, 0);
+            tableLayoutPanel_topSection.Controls.Add(textBox_passwordRCON, 1, 2);
+            tableLayoutPanel_topSection.Controls.Add(button1, 0, 2);
+            tableLayoutPanel_topSection.Name = "tableLayoutPanel_topSection";
+            // 
+            // textBox_ipRCON
+            // 
+            resources.ApplyResources(textBox_ipRCON, "textBox_ipRCON");
+            textBox_ipRCON.Name = "textBox_ipRCON";
+            // 
+            // textBox_portRCON
+            // 
+            resources.ApplyResources(textBox_portRCON, "textBox_portRCON");
+            textBox_portRCON.Name = "textBox_portRCON";
             // 
             // button_connectRCON
             // 
             resources.ApplyResources(button_connectRCON, "button_connectRCON");
             button_connectRCON.Name = "button_connectRCON";
             button_connectRCON.UseVisualStyleBackColor = true;
+            button_connectRCON.Click += button_connectRCON_Click;
             // 
             // button_disconnectRCON
             // 
             resources.ApplyResources(button_disconnectRCON, "button_disconnectRCON");
             button_disconnectRCON.Name = "button_disconnectRCON";
             button_disconnectRCON.UseVisualStyleBackColor = true;
+            button_disconnectRCON.Click += button_disconnectRCON_Click;
             // 
-            // textBox3
+            // textBox_passwordRCON
             // 
-            resources.ApplyResources(textBox3, "textBox3");
-            textBox3.Name = "textBox3";
+            resources.ApplyResources(textBox_passwordRCON, "textBox_passwordRCON");
+            textBox_passwordRCON.Name = "textBox_passwordRCON";
+            // 
+            // button1
+            // 
+            resources.ApplyResources(button1, "button1");
+            button1.Name = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // Form_RCON
             // 
@@ -122,25 +204,38 @@ namespace PalworldServerManager
             Name = "Form_RCON";
             Load += Form_RCON_Load;
             panel1.ResumeLayout(false);
-            panel1.PerformLayout();
-            tableLayoutPanel2.ResumeLayout(false);
-            tableLayoutPanel2.PerformLayout();
-            tableLayoutPanel3.ResumeLayout(false);
-            tableLayoutPanel3.PerformLayout();
+            panel2.ResumeLayout(false);
+            tableLayoutPanel_sendSection.ResumeLayout(false);
+            tableLayoutPanel_sendSection.PerformLayout();
+            panel_playersSection.ResumeLayout(false);
+            tableLayoutPanel1.ResumeLayout(false);
+            panel3.ResumeLayout(false);
+            tableLayoutPanel_topSection.ResumeLayout(false);
+            tableLayoutPanel_topSection.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.RichTextBox richTextBox_output;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel_sendSection;
         private System.Windows.Forms.TextBox textBox_commandText;
         private System.Windows.Forms.Button button_sendCommand;
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel_topSection;
+        private System.Windows.Forms.TextBox textBox_ipRCON;
+        private System.Windows.Forms.TextBox textBox_portRCON;
         private System.Windows.Forms.Button button_connectRCON;
         private System.Windows.Forms.Button button_disconnectRCON;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox_passwordRCON;
+        private System.Windows.Forms.Panel panel_playersSection;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel_playerListSection;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button_kick;
+        private System.Windows.Forms.Button button_ban;
+        private System.Windows.Forms.ToolTip toolTip1;
     }
 }
--- PalworldServerManager/PalworldServerManager/Form_RCON.cs
+++ PalworldServerManager/PalworldServerManager/Form_RCON.cs
@@ -1,21 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.Design;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Net.Sockets;
+using System.Reflection.PortableExecutable;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using RconSharp;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace PalworldServerManager
 {
     public partial class Form_RCON : Form
     {
-        private string serverIP = "127.0.0.1";
-        private int rconPort = 25575;
-        private string rconPassword = "";
+        private RconClient _rconClient;
+        string ipRCON;
+        int portRCON;
+        string passwordRCON;
+        string selectedIGN;
+        string selectedSteamID;
 
         public Form_RCON()
 
@@ -25,6 +32,234 @@ namespace PalworldServerManager
 
         private void Form_RCON_Load(object sender, EventArgs e)
         {
+            button_connectRCON.Enabled = true;
+            button_disconnectRCON.Enabled = false;
+        }
+
+        private async void ConnectRCON(string ipRcon, int portRcon, string passwordRcon)
+        {
+            
+            try
+            {
+                // Open the connection
+                 _rconClient.ConnectAsync();
+                //richTextBox_output.AppendText("Connected to RCON server." + Environment.NewLine);
+            }
+            catch (Exception ex)
+            {
+                richTextBox_output.AppendText($"Failed to connect: {ex.Message}" + Environment.NewLine);
+            }
+
+            try {
+                // Send a RCON packet with type AUTH and the RCON password for the target server
+                bool authenticated = await _rconClient.AuthenticateAsync(passwordRCON);
+                richTextBox_output.AppendText("Validating Password..." + Environment.NewLine);
+                if (authenticated)
+                {
+                    string info = await _rconClient.ExecuteCommandAsync("info");
+                    richTextBox_output.AppendText(info + Environment.NewLine);
+                    GetPlayerList();
+                    button_connectRCON.Enabled = false;
+                    button_disconnectRCON.Enabled = true;
+                }
+                else
+                {
+                    richTextBox_output.AppendText("Incorrect Password" + Environment.NewLine);
+                }
+            }
+            catch (Exception ex) { richTextBox_output.AppendText($"Error: {ex.Message}" + Environment.NewLine); }
+
+            
+        }
+
+        private async void DisconnectRCON()
+        {
+            try
+            {
+
+                if (_rconClient != null)
+                {
+                    _rconClient.Disconnect();
+                    richTextBox_output.AppendText($"Disconnected" + Environment.NewLine);
+                    button_connectRCON.Enabled = true;
+                    button_disconnectRCON.Enabled = false;
+                }
+                else
+                {
+                    //richTextBox_output.AppendText($"Not Connecetd To RCON" + Environment.NewLine);
+                }
+            }
+            catch (Exception ex)
+            {
+                richTextBox_output.AppendText($"Error: {ex.Message}" + Environment.NewLine);
+            }
+        }
+
+        private async void button_connectRCON_Click(object sender, EventArgs e)
+        {
+            if (textBox_ipRCON.Text == "" || textBox_portRCON.Text == "" || textBox_passwordRCON.Text == "")
+            {
+                richTextBox_output.AppendText("Ops, please fill in all rcon info before connecting to rcon" + Environment.NewLine);
+                return;
+            }
+            
+            ipRCON = textBox_ipRCON.Text;
+            portRCON = int.Parse(textBox_portRCON.Text);
+            passwordRCON = textBox_passwordRCON.Text;
+            _rconClient = RconClient.Create(ipRCON, portRCON);
+            ConnectRCON(ipRCON, portRCON, passwordRCON);
+
+
+
+        }
+
+        private async void GetPlayerList()
+        {
+            string listOfPlayers = await _rconClient.ExecuteCommandAsync("showplayers");
+            //string listOfPlayers = "Player1\nPlayer2\nPlayer3\nPlayer4\nPlayer5\nPlayer6\nPlayer7\nPlayer8\nPlayer9\nPlayer10\nPlayer11\nPlayer12\nPlayer13\nPlayer14\nPlayer15\nPlayer16\nPlayer17\nPlayer18\nPlayer19\nPlayer20\nPlayer21\nPlayer22\nPlayer23\nPlayer24\nPlayer25\nPlayer26\nPlayer27\nPlayer28\nPlayer29\nPlayer30\nPlayer31\nPlayer32\n";
+            // Clear existing buttons if any
+            panel_playerListSection.Controls.Clear();
+            // Split the player list string into individual player names
+            string[] players = listOfPlayers.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+            // Create and add buttons for each player except the line on the list
+            for (int i = 1; i < players.Length; i++)
+            {
+                Button button = new Button();
+                button.Text = players[i];
+                button.Dock = DockStyle.Top;
+                button.Click += SelectedPlayer_Click; // Subscribe to Click event
+                panel_playerListSection.Controls.Add(button);
+            }
+
+        }
+        private void SelectedPlayer_Click(object sender, EventArgs e)
+        {
+            // Handle button click event here
+            Button clickedButton = (Button)sender;
+            // Split the text by commas and take the last part
+            string[] parts = clickedButton.Text.Split(',');
+            selectedIGN = parts[0].Trim();
+            selectedSteamID = parts[parts.Length - 1].Trim(); // Trim removes any leading or trailing spaces
+            Clipboard.SetText(selectedSteamID);
+            // Show a quick pop-up notification
+            toolTip1.Show($"Copied {selectedIGN}'s SteamID to clipboard", clickedButton, 0, 0, 1000); // Display for 1 second
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Test Button
+            GetPlayerList();
+        }
+
+        private async void button_sendCommand_Click(object sender, EventArgs e)
+        {
+            //TO BE WORKED ON
+            try
+            {
+                if (_rconClient != null)
+                {
+                    string sendCommand = await _rconClient.ExecuteCommandAsync(textBox_commandText.Text);
+                    richTextBox_output.AppendText(sendCommand.ToString() + Environment.NewLine);
+                    textBox_commandText.Clear();
+                }
+                else
+                {
+                    richTextBox_output.AppendText($"Not Connecetd To RCON" + Environment.NewLine);
+                }
+            }
+            catch (TaskCanceledException tex)
+            {
+                richTextBox_output.AppendText($"TaskCanceledException Error: {tex.Message} " + Environment.NewLine);
+            }
+            catch (NullReferenceException nex)
+            {
+                richTextBox_output.AppendText($"NullReferenceException Error: {nex.Message} " + Environment.NewLine);
+            }
+            catch (Exception ex)
+            {
+                richTextBox_output.AppendText($"Error: {ex.Message}" + Environment.NewLine);
+            }
+
+
+            //if (_rconClient != null)
+            //{
+            //    try
+            //    {
+            //        var sendCommand = await _rconClient.ExecuteCommandAsync(textBox_commandText.Text);
+            //        richTextBox_output.AppendText(sendCommand.ToString() + Environment.NewLine);
+            //        textBox_commandText.Clear();
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        richTextBox_output.AppendText($"Error: {ex.Message}" + Environment.NewLine);
+            //        DisconnectRCON();
+            //        ConnectRCON(ipRCON, portRCON, passwordRCON);
+
+            //    }
+            //}
+            //else
+            //{
+            //    richTextBox_output.AppendText($"Not Connecetd To RCON" + Environment.NewLine);
+            //}
+               
+
+        }
+
+        private async void button_kick_Click(object sender, EventArgs e)
+        {
+            
+            if (selectedSteamID != null)
+            {
+                try
+                {
+                    DialogResult confirmation = MessageBox.Show($"KICK\n Player: {selectedIGN} SteamID: {selectedSteamID}", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                    if (confirmation == DialogResult.Yes)
+                    {
+                        string kickPlayer = await _rconClient.ExecuteCommandAsync($"KickPlayer {selectedSteamID}");
+                        richTextBox_output.AppendText($"KICKED Player: {selectedIGN} SteamID: {selectedSteamID} " + Environment.NewLine);
+
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    //Error handling //Do it later
+                    richTextBox_output.AppendText($"Error: {ex.Message}" + Environment.NewLine);
+                }
+            }
+        }
+
+        private async void button_ban_Click(object sender, EventArgs e)
+        {
+            if (selectedSteamID != null)
+            {
+                try
+                {
+                    DialogResult confirmation = MessageBox.Show($"BAN\n Player: {selectedIGN} SteamID: {selectedSteamID}", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                    if (confirmation == DialogResult.Yes)
+                    {
+                        string banPlayer = await _rconClient.ExecuteCommandAsync($"KickPlayer {selectedSteamID}");
+                        richTextBox_output.AppendText($"BANNED Player: {selectedIGN} SteamID: {selectedSteamID} " + Environment.NewLine);
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    //Error handling //Do it later
+                    richTextBox_output.AppendText($"Error: {ex.Message}" + Environment.NewLine);
+                }
+            }
+        }
+
+        private void button_disconnectRCON_Click(object sender, EventArgs e)
+        {
+
+            DisconnectRCON();
+
+
         }
     }
 }
